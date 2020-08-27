d4935376e417480e0bd7028b5126ae5df30e3176
Daniel <danielheinze96@gmail.com>
2020-08-27 17:54:34 +0200

    adding docer-compose

--- src/connect.php
+++ src/connect.php
@@ -1,9 +1,10 @@
 <?php
 
-$host ="localhost";
+$host = getenv('DB_HOST_NAME') ?? 'localhost';
 $db_user = "14760_menago";
 $db_password = "Rv8K@79sIA!0D";
 $db_name ="14760_menago";
+
 
 
 
--- src/connectPDO.php
+++ src/connectPDO.php
@@ -1,5 +1,6 @@
 <?php
+$host = getenv('DB_HOST_NAME') ?? 'localhost';
 $password = "Rv8K@79sIA!0D";
-$dsn = 'mysql:host=localhost;dbname=14760_menago;charset=utf8';
+$dsn = 'mysql:host={$host};dbname=14760_menago;charset=utf8';
 $username = '14760_menago';
 
